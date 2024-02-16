ad5b136fea3e054de90c8a3aaf1cc60585718bbd
Sigurd015 <yujiang015@gmail.com>
2024-02-16 23:48:11 +0900

    Added Scene
    - Implement multithreading path tracing

--- Raytracing/src/Renderer.h
+++ Raytracing/src/Renderer.h
@@ -3,6 +3,7 @@
 #include "Walnut/Image.h"
 #include "Camera.h"
 #include "Ray.h"
+#include "Scene.h"
 
 #include <memory>
 #include <glm/glm.hpp>
@@ -10,14 +11,43 @@
 class Renderer
 {
 public:
+	struct Settings
+	{
+		bool Accumulate = true;
+	};
 	Renderer() = default;
 	void OnResize(uint32_t width, uint32_t height);
-	void Render(const Camera& camera);
+	void Render(const Scene& scene, const Camera& camera);
 	std::shared_ptr<Walnut::Image> GetFinalImage() const { return m_FinalImage; }
+	void ResetFrameIndex() { m_FrameIndex = 1; }
+	Settings& GetSettings() { return m_Settings; }
 
 private:
-	glm::vec4 TraceRay(const Ray& ray);
+	struct HitPayload
+	{
+		float HitDistance;
+		glm::vec3 WorldPosition;
+		glm::vec3 WorldNormal;
+
+		int ObjectIndex;
+	};
+
+	glm::vec4 PerPixel(uint32_t x, uint32_t y); // RayGen
+
+	HitPayload TraceRay(const Ray& ray);
+	HitPayload ClosestHit(const Ray& ray, float hitDistance, int objectIndex);
+	HitPayload Miss(const Ray& ray);
+
+	const Scene* m_ActiveScene = nullptr;
+	const Camera* m_ActiveCamera = nullptr;
+
+	Settings m_Settings;
 
 	std::shared_ptr<Walnut::Image> m_FinalImage;
 	uint32_t* m_ImageData = nullptr;
+	glm::vec4* m_AccumulationData = nullptr;
+
+	std::vector<uint32_t> m_ImageHorizontalIter, m_ImageVerticalIter;
+
+	uint32_t m_FrameIndex = 1;
 };
